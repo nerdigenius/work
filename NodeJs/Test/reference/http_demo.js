f4d85c9de799bc772cc8e742717bb824e0a79803
nerdigenius <Ashiq.racer@gmail.com>
2019-05-27 14:49:23 +0600

    00

--- NodeJs/Test/reference/http_demo.js
+++
@@ -1,9 +0,0 @@
-const http = require ('http');
-
-
-//Create server object
-http.createServer((req,res)=>{
-    //Write response
-    res.write('Hello World');
-    res.end();
-}).listen(process.env.PORT,()=> console.log('Server running...'));
