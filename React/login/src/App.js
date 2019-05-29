fa354f136861fa9d56cd5a321d921ce8c7d8f1b2
nerdigenius <Ashiq.racer@gmail.com>
2019-05-29 16:24:24 +0600

    new login in react

---
+++ React/login/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.png'
+
+function App() {
+  return (
+    <div className="loginbox" >
+      <img src={logo}   alt="Logo" className="avatar"/>
+          
+    </div>
+  );
+}
+
+export default App;
